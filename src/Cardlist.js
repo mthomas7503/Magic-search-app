d1e4f75eae566455df1b68ce394f1f8dbc96d99d
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-10-23 12:17:35 -0400

    update CSS and add condition message

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -4,14 +4,19 @@ import { FaList } from "react-icons/fa"
 
 
 function Cardlist({ list, handleDecklist }) {
-    
-    return(
+    if(list.length === 0) {
+        return(<div className="cardlistholder">
+            <h1>Add to your Decklist from Search!</h1>
+        </div>)
+    }
+
+    else {return(
         <div className = {list.length < 3 ? "cardlistholder" : "cardlist"}>
             {list.map((cardData, index) => {
                 return(<Card key={index} cardObj={cardData} handleDecklist={handleDecklist}/>)}
             )}
         </div>
-    )
+    )}
 }
 
 export default Cardlist
